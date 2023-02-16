326cbb48615a0927643446582f032ae14b18b7d1
zeeshan <zeeshanbutt223@gmail.com>
2023-02-16 15:16:30 +0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  console.log('Cookies: ', req.cookies)
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+
   res.render('index', { title: 'Express' });
 });
 
